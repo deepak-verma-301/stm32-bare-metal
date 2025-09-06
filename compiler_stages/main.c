46311d7819eef4c9eb8f1b995eda601b9f7d8a22
Mrbot <deepakver301@gmail.com>
2025-09-06 08:49:09 +0530

    creating a sample main.c file

---
+++ compiler_stages/main.c
@@ -0,0 +1,10 @@
+#define pin 13       //this macro is remove at the first stage of compilation 
+                    // all the comments will also be removed after first stage of compilation 
+
+
+int main(){
+
+int led = pin;
+
+	return 0;
+}
